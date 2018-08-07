14892e035d28ae2773e521d54972d81fea1e3516
Leonardo Dylan da Silva <leonardo.cazuza98@gmail.com>
2018-08-07 07:55:32 -0300

    comentario

---
+++ scriptemailagenda.py
@@ -0,0 +1,24 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+email = 'pythontestemeli@gmail.com'
+password = 'Meli1234'
+send_to_email =' paula.nobre@mercadolivre.com'
+#agenda = 
+subject = 'Teste'
+message = 'Testando topper'
+
+msg = MIMEMultipart()
+msg['From'] = email
+msg['To'] = send_to_email
+msg['Subject'] = subject
+
+msg.attach(MIMEText(message, 'plain'))
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(email, password)
+text = msg.as_string()
+server.sendmail(email, send_to_email, text)
+server.quit()
+
---
+++ scriptraspmouse.py
@@ -0,0 +1,9 @@
+from pynput.mouse import Button, Controller
+mouse=Controller()
+
+a = 0
+while(a == 0);
+	mouse.move(1139,308)
+	mouse.move(1300,394)
+	mouse.move(1321,403)
+	
