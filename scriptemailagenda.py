9531b15c6ae56f61fc90b472eb93f55d9a5e8161
Leonardo Dylan da Silva <leonardo.cazuza98@gmail.com>
2018-08-07 09:47:43 -0300

    adicionado print

--- scriptemailagenda.py
+++ scriptemailagenda.py
@@ -3,7 +3,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 email = 'pythontestemeli@gmail.com'
 password = 'Meli1234'
-send_to_email =' paula.nobre@mercadolivre.com'
+send_to_email ='leonardo.dylan@mercadolivre.com'
 #agenda = 
 subject = 'Teste'
 message = 'Testando topper'
@@ -22,3 +22,4 @@ text = msg.as_string()
 server.sendmail(email, send_to_email, text)
 server.quit()
 
+print("E-mail enviado com sucesso") 
